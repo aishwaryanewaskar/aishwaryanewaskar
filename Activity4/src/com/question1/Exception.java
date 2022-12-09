ad9ebd410db2afbe5f15070007905f475f9b92fe
aishw <aishw@DESKTOP-NPJ659M>
2022-12-09 20:42:07 +0530

    Activity 4

--- Activity4/src/com/question1/Exception.java
+++ Activity4/src/com/question1/Exception.java
@@ -4,29 +4,35 @@ import java.util.Scanner;
 
 public class Exception {
 
-	public static void main(String[] args) 
-{
-		// declare and initialize here
-					int p,q,r;
-					Scanner sc=new Scanner(System.in);
-					//  asks the user to input two numbers
-					System.out.println("Enter 1st number : ");
-					p=sc.nextInt();
-					System.out.println("Enter 2nd number : ");
-					q=sc.nextInt();			
-				try
-				{
-					//first number is then divided by the second number and display the result on screen
-					r=p/q;
-					System.out.println("Result:"+r);
-				}
-				catch(ArithmeticException e)
-				{
-					//Arithmetic Exception is thrown when user try to divide by zero
-					System.out.println("Arithmetic Exception occured");
-				}
-			}
-		
+	public static void main(String[] args) throws Exception {
+      int a,b,result;
+      Scanner s=new Scanner(System.in);
+      System.out.println("enter a");
+      a=s.nextInt();
+      System.out.println("enter b");
+      b=s.nextInt();
+      if(a==0 && b==0)
+      {
+    	  throw new Exception("enter two numbers");
+      }
+      else
+      {
+    	  try
+    	  {
+    		  result=a/b;
+    		  System.out.println("result is"+result);
+    	  }
+    	  catch(ArithmaticException e)
+    	  {
+    		  System.out.println("divide by zero error");
+    	  }
+      }
+	}
+	finally
+	{
+		System.out.println("successful division");
+	}
 }
+
 
 
